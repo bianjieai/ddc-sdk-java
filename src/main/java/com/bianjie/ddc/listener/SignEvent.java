b7116589c0f4e4e231739fe38099e92ac5fcb7a3
ajiang55 <a550374@163.com>
2021-12-29 16:03:22 +0800

    init

---
+++ src/main/java/com/bianjie/ddc/listener/SignEvent.java
@@ -0,0 +1,15 @@
+package com.bianjie.ddc.listener;
+
+public class SignEvent {
+
+	private String encodeTransaction;
+	
+	public String getEncodeTransaction() {
+		return encodeTransaction;
+	}
+
+	public void setEncodeTransaction(String encodeTransaction) {
+		this.encodeTransaction = encodeTransaction;
+	}
+	
+}
