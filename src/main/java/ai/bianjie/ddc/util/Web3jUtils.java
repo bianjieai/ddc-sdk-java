ad711728f374f00d82acc1d57a94c67b52a0a376
ysm <2773105748@qq.com>
2022-01-12 00:50:10 -0800

    web3util

--- src/main/java/ai/bianjie/ddc/util/Web3jUtils.java
+++ src/main/java/ai/bianjie/ddc/util/Web3jUtils.java
@@ -7,9 +7,6 @@ import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 
 public class Web3jUtils {
-    private Web3j web3j = Web3j.build(new HttpService(ConfigCache.get().getOpbGatewayAddress()));
-
-    private Credentials credentials = Credentials.create(ConfigCache.get().getCredentials());
 
     public static AuthorityLogic getAuthority() {
         return AuthorityLogic.load(ConfigCache.get().getAuthorityLogicAddress(), Web3j.build(new HttpService(ConfigCache.get().getOpbGatewayAddress())), Credentials.create(ConfigCache.get().getCredentials()), new GasProvider());
