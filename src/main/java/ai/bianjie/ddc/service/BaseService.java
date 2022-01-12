1dc46816b47bb24313451d9bb22b0d4f5286a2f8
ysm <2773105748@qq.com>
2022-01-11 18:42:09 -0800

    baseservice

--- src/main/java/ai/bianjie/ddc/service/BaseService.java
+++ src/main/java/ai/bianjie/ddc/service/BaseService.java
@@ -1,15 +1,11 @@
 package ai.bianjie.ddc.service;
 
-import ai.bianjie.ddc.constant.ErrorMessage;
-import ai.bianjie.ddc.exception.DDCException;
 import ai.bianjie.ddc.listener.SignEventListener;
 import ai.bianjie.ddc.util.Web3jUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
-import org.web3j.protocol.core.methods.response.EthBlock;
-import org.web3j.protocol.core.methods.response.EthGetTransactionReceipt;
-import org.web3j.protocol.core.methods.response.EthTransaction;
+import org.web3j.protocol.core.methods.response.*;
 import org.web3j.utils.Strings;
 
 import java.io.IOException;
@@ -38,10 +34,10 @@ public class BaseService {
      * @return 交易回执
      * @throws InterruptedException InterruptedException
      */
-    public EthGetTransactionReceipt getTransReceipt(String hash) throws InterruptedException, ExecutionException {
+    public TransactionReceipt getTransReceipt(String hash) throws InterruptedException, ExecutionException {
         Web3jUtils web3jUtils = new Web3jUtils();
         Web3j web3j = web3jUtils.getWeb3j();
-        return web3j.ethGetTransactionReceipt(hash).sendAsync().get();
+        return web3j.ethGetTransactionReceipt(hash).sendAsync().get().getTransactionReceipt().get();
     }
 
     /**
@@ -49,10 +45,10 @@ public class BaseService {
      * @param hash 交易哈希
      * @return 交易信息
      */
-    public EthTransaction getTransByHash(String hash) throws IOException {
+    public Transaction getTransByHash(String hash) throws IOException {
         Web3jUtils web3jUtils = new Web3jUtils();
         Web3j web3j = web3jUtils.getWeb3j();
-        return web3j.ethGetTransactionByHash(hash).send();
+        return web3j.ethGetTransactionByHash(hash).send().getTransaction().get();
     }
 
     /**
@@ -63,7 +59,7 @@ public class BaseService {
     public Boolean getTransByStatus(String hash) throws ExecutionException, InterruptedException {
         Web3jUtils web3jUtils = new Web3jUtils();
         Web3j web3j = web3jUtils.getWeb3j();
-        EthGetTransactionReceipt txReceipt = web3j.ethGetTransactionReceipt(hash).sendAsync().get();
+        TransactionReceipt txReceipt = web3j.ethGetTransactionReceipt(hash).sendAsync().get().getTransactionReceipt().get();
         return !Strings.isEmpty(txReceipt.toString());
 
     }
