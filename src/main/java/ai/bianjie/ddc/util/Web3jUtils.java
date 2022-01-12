f2989b46940b0b1e63278b3ccec07d9ce337da33
ajiang55 <a550374@163.com>
2022-01-12 15:02:22 +0800

    client update

--- src/main/java/ai/bianjie/ddc/util/Web3jUtils.java
+++ src/main/java/ai/bianjie/ddc/util/Web3jUtils.java
@@ -9,11 +9,21 @@ import org.web3j.protocol.http.HttpService;
 public class Web3jUtils {
     public static AuthorityLogic getAuthority() {
         return AuthorityLogic.load(ConfigCache.get().getAuthorityLogicAddress(), Web3j.build(new HttpService(ConfigCache.get().getOpbGatewayAddress())), Credentials.create(ConfigCache.get().getCredentials()), new GasProvider());
-    }public static ChargeLogic getCharge() {
+    }
+
+    public static ChargeLogic getCharge() {
         return ChargeLogic.load(ConfigCache.get().getChargeLogicAddress(), Web3j.build(new HttpService(ConfigCache.get().getOpbGatewayAddress())), Credentials.create(ConfigCache.get().getCredentials()), new GasProvider());
-    }public static DDC1155 getDDC1155() {
+    }
+
+    public static DDC1155 getDDC1155() {
         return DDC1155.load(ConfigCache.get().getDdc1155Address(), Web3j.build(new HttpService(ConfigCache.get().getOpbGatewayAddress())), Credentials.create(ConfigCache.get().getCredentials()), new GasProvider());
-    }public static DDC721 getDDC721() {
+    }
+
+    public static DDC721 getDDC721() {
         return DDC721.load(ConfigCache.get().getDdc721Address(), Web3j.build(new HttpService(ConfigCache.get().getOpbGatewayAddress())), Credentials.create(ConfigCache.get().getCredentials()), new GasProvider());
-    }public static Web3j getWeb3j() { return Web3j.build(new HttpService(ConfigCache.get().getOpbGatewayAddress())); }
+    }
+
+    public static Web3j getWeb3j() {
+        return Web3j.build(new HttpService(ConfigCache.get().getOpbGatewayAddress()));
+    }
 }
