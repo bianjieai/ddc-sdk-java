6c67c98d0601c98cf1a437581dcec14e52f76f9c
ajiang55 <a550374@163.com>
2022-01-13 15:56:15 +0800

    BlockEventService update

--- src/main/java/ai/bianjie/ddc/constant/DDC721Functions.java
+++ src/main/java/ai/bianjie/ddc/constant/DDC721Functions.java
@@ -23,7 +23,7 @@ public class DDC721Functions {
     public static final String DDC_URI = "ddcURI";
 
     //public static final String DDC721TransferEvent = "Transfer(address,address,uint256)";
-    public static final String DDC721TransferEvent = "0xe1aaf522946f19bd4a0b6e67a2da36e45fcddca4a1a253a919ff7029a638ab16";
+    public static final String DDC721TransferEvent = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
     //public static final String DDC721FreezeEvent = "EnterBlacklist(address,uint256)";
     public static final String DDC721FreezeEvent = "0x027b0995c9aa454830a50ece99b9507432deb5f7ff0173efc4429282c1710a36";
     //public static final String DDC721UnFreezeEvent = "ExitBlacklist(address,uint256)";
