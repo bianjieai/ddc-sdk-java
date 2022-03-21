67f9615bcc4c423ef2e73371d284009bf07bf1ef
ht <22350747@qq.com>
2022-03-21 17:05:10 +0800

    修改异常

--- ddc-sdk-platform-java/src/main/java/ai/bianjie/ddc/service/BaseService.java
+++ ddc-sdk-platform-java/src/main/java/ai/bianjie/ddc/service/BaseService.java
@@ -83,7 +83,7 @@ public class BaseService {
      *
      * @param hash 交易哈希
      * @return 交易状态
-     * @throws ExecutionException
+     * @throws IOException
      */
     public Boolean getTransByStatus(String hash) throws IOException {
         return Web3jUtils.getWeb3j().ethGetTransactionReceipt(hash).send().getTransactionReceipt().get().isStatusOK();
