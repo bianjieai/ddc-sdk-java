da73ed4a05f61504091b1f90984df6f308dc9c14
shimin <shimin.bianjie.ai>
2022-05-31 18:47:55 +0800

    develop

--- ddc-sdk-operator-java/src/main/java/ai/bianjie/ddc/config/ConfigInfo.java
+++ ddc-sdk-operator-java/src/main/java/ai/bianjie/ddc/config/ConfigInfo.java
@@ -1,10 +1,5 @@
 package ai.bianjie.ddc.config;
 
-import ai.bianjie.ddc.contract.Authority;
-import ai.bianjie.ddc.contract.Charge;
-import ai.bianjie.ddc.contract.DDC1155;
-import ai.bianjie.ddc.contract.DDC721;
-
 import java.util.HashMap;
 import java.util.Map;
 
